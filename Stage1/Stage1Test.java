78b5227f9d12beeeee14cbdffd06ee98610856b9
tuma666 <leonardo.castillot@sansano.usm.cl>
2020-05-16 14:05:09 -0400

    LeonardC90

--- Stage1/Stage1Test.java
+++ Stage1/Stage1Test.java
@@ -1,33 +1,29 @@
 import java.io.*;
 import java.util.Scanner;
 import java.util.Locale;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.math.*;
+import java.text.DecimalFormat;
 
 public class Stage1Test  {
    public static void main (String[] arg) throws IOException {
       Locale.setDefault(Locale.US);  // to read number in US format, like 1.5 (not like 1,5)
       Scanner in = new Scanner(new File(arg[0]));
-      BufferedWriter writer = new BufferedWriter(new FileWriter(arg[1]);
-      writer.append("t,jih,jiv,jdh,jdv");
-      writer.newLine();
+      File archivo= new File(arg[1]);
+      archivo.createNewFile();
+      FileWriter doc= new FileWriter(archivo);
+      
       float time = 0.0f;
       Joystick l_Joy = new Joystick();
       Joystick r_Joy = new Joystick();
       Operator operator = new Operator(in, l_Joy, r_Joy);
-      while(operator.takeAction(time)) { 
-
-         writer.append( Math.floor(time*10)/10 +","+l_Joy.toString()+","+r_Joy.toString());
-         writer.newLine();
-
-         System.out.println(Math.floor(time*10)/10 +" "+l_Joy.toString()+" "+r_Joy.toString());
+      DecimalFormat num= new DecimalFormat("#0.0");
+      while(operator.takeAction(time)) {
+         
+         System.out.println(String.format("%.1f",time)+" "+l_Joy.toString()+" "+r_Joy.toString());
+         doc.write(String.format("%.1f",time)+" "+l_Joy.toString()+" "+r_Joy.toString()+"\n");
          time= (float) (time + 0.1);
       }
-      write.flush();
-      writer.close();
-      
+      doc.flush();
+      doc.close();
+
    }
-   
-   
 }
